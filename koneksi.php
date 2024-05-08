e6e7b772d4731aeaa52f43dbbdfbcfd6dfe439ac
Amelia Vega <133181467+AmeliaVegaa@users.noreply.github.com>
2024-05-08 18:11:09 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
     $namahost = "localhost";
     $username = "root";
-    $password = "";
+    $password = "2612";
     $database = "map";
     $con = mysqli_connect($namahost, $username, $password, $database) or die ("Failed");
 ?>
